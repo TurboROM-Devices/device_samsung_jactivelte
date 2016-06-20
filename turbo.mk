8f840861a9c46a79dc13e8719bfb3aa4652f9f4e
Yahya Wessam <vbajs@users.noreply.github.com>
2016-06-20 21:48:20 +0300

    Turbo-ify

---
+++ turbo.mk
@@ -0,0 +1,20 @@
+$(call inherit-product, device/samsung/jactivelte/full_jactivelte.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/turbo/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/turbo/config/common_full_phone.mk)
+
+# Gello
+PRODUCT_PACKAGES += \
+    Gello
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=jactivelte \
+    TARGET_DEVICE=jactivelte \
+    BUILD_FINGERPRINT="samsung/jactivelte/jactivelte:5.0.1/LRX22C/I9295XXUDOI5:user/release-keys" \
+    PRIVATE_BUILD_DESC="jactivelte-user 5.0.1 LRX22C I9295XXUDOI5 release-keys"
+
+PRODUCT_NAME := turbo_jactivelte
+PRODUCT_DEVICE := jactivelte
